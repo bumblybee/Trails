c5cb58b4d68caace15a14aac0c60656b55ad90dd
Tiffani Hess <hesstjune@gmail.com>
2020-11-13 15:17:57 -0600

    brought in he to decode html entities in trail description

--- src/components/search/list/TrailCard.js
+++ src/components/search/list/TrailCard.js
@@ -1,11 +1,17 @@
 import React from "react";
+import he from "he";
 import * as sc from "./StyledTrailList";
 
 //TODO: Difficulty icons
 //TODO: Length icon and #
 //TODO: Rating icon (out of 5) add to card footer
 const TrailCard = ({ trail }) => {
-  // console.log(trail);
+  const decodeHTML = (html) => {
+    var txt = document.createElement("p");
+    txt.innerHTML = html;
+    return txt.value;
+  };
+
   return (
     <sc.StyledCard>
       <sc.StyledImageContainer>
@@ -26,7 +32,7 @@ const TrailCard = ({ trail }) => {
           <h3>{trail.name}</h3>
           <h5>{trail.city}</h5>
         </div>
-        <p>{trail.description.substring(0, 200)}...</p>
+        <p>{he.decode(trail.description).substring(0, 220)}...</p>
       </sc.StyledCardContentContainer>
     </sc.StyledCard>
   );
