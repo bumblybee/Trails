f9bde498668ee527503c69e6b954b1560a9c0263
Tiffani Hess <hesstjune@gmail.com>
2020-11-15 11:07:52 -0600

    moved TrailListHeader into TrailList so they scroll together

--- src/components/search/list/StyledTrailList.js
+++ src/components/search/list/StyledTrailList.js
@@ -1,26 +1,27 @@
 import styled from "styled-components";
 
+export const StyledTrailList = styled.ul`
+  padding: 2rem 1rem 1rem 1.5rem;
+  overflow-y: scroll;
+  max-height: 874px;
+  ::-webkit-scrollbar-scrollbar-track-color {
+    background: transparent;
+    color: transparent;
+  }
+`;
+
 export const StyledTrailListHeader = styled.div`
   color: #34423b;
-  padding: 0.5rem 1rem;
+  padding: 0 0 2rem;
 
   h1 {
-    margin: 1rem 0 0.5rem;
+    margin: 0 0 0.5rem;
   }
 
   p {
     color: #99a29e;
     font-size: 0.9rem;
     font-weight: 400;
-  }
-`;
-
-export const StyledTrailList = styled.ul`
-  padding: 1rem;
-  overflow-y: scroll;
-  max-height: 874px;
-  ::-webkit-scrollbar-scrollbar-track-color {
-    background: transparent;
-    color: transparent;
+    margin-left: 0.2rem;
   }
 `;
--- src/components/search/list/TrailList.js
+++ src/components/search/list/TrailList.js
@@ -8,11 +8,13 @@ const TrailList = () => {
   const { trails, searchValue } = useContext(SearchContext);
   return (
     <div>
-      <StyledTrailListHeader>
-        <h1>Trails near {searchValue.substring(0, searchValue.length - 5)}</h1>
-        <p>{trails.length} trails</p>
-      </StyledTrailListHeader>
       <StyledTrailList>
+        <StyledTrailListHeader>
+          <h1>
+            Trails near {searchValue.substring(0, searchValue.length - 5)}
+          </h1>
+          <p>{trails.length} trails</p>
+        </StyledTrailListHeader>
         {trails &&
           trails.map((trail, index) => <TrailCard key={index} trail={trail} />)}
       </StyledTrailList>
