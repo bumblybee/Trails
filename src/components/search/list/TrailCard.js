40aaa10c93567464073f558185ebb390bbf2c33e
Tiffani Hess <hesstjune@gmail.com>
2020-11-30 14:20:49 -0600

    brought in react icons and replaced other icons, still need to style

--- src/components/search/list/TrailCard.js
+++ src/components/search/list/TrailCard.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
 import he from "he";
+import {
+  FaRoute,
+  FaBookmark,
+  FaRegBookmark,
+  FaHiking,
+  FaBiking,
+  FaMedal,
+} from "react-icons/fa";
 
 import * as sc from "./StyledTrailCard";
 
@@ -9,55 +17,6 @@ import * as sc from "./StyledTrailCard";
 //TODO: check if need he decode now that using regex on server side
 const TrailCard = ({ trail }) => {
   const [hover, setHover] = useState(false);
-  const renderDifficultyIcon = () => {
-    if (trail.difficulty === "unknown")
-      return (
-        <img
-          title={`difficulty ${trail.difficulty}`}
-          alt="difficulty"
-          src="https://img.icons8.com/material-outlined/24/D0D4D2/speedometer.png"
-        />
-      );
-
-    if (trail.difficulty === "beginner")
-      return (
-        <img
-          title="beginner"
-          alt="difficulty"
-          src="https://img.icons8.com/material-outlined/24/b1ce7c/speedometer.png"
-        />
-      );
-
-    if (trail.difficulty === "intermediate") {
-      return (
-        <img
-          title={trail.difficulty}
-          alt="difficulty"
-          src="https://img.icons8.com/material-outlined/24/749073/speedometer.png"
-        />
-      );
-    }
-
-    if (trail.difficulty === "advanced") {
-      return (
-        <img
-          title={trail.difficulty}
-          alt="difficulty"
-          src="https://img.icons8.com/material-outlined/24/FE7762/speedometer.png"
-        />
-      );
-    }
-
-    if (trail.difficulty === "expert") {
-      return (
-        <img
-          title={trail.difficulty}
-          alt="difficulty"
-          src="https://img.icons8.com/material-outlined/24/FE8A62/speedometer.png"
-        />
-      );
-    }
-  };
 
   const countChars = () => {
     const desc = trail.description;
@@ -80,19 +39,7 @@ const TrailCard = ({ trail }) => {
           onMouseEnter={() => setHover(true)}
           onMouseLeave={() => setHover(false)}
         >
-          {hover ? (
-            <img
-              title="bookmark trail"
-              alt="bookmark icon"
-              src="https://img.icons8.com/material/28/fe7762/bookmark-ribbon--v1.png"
-            />
-          ) : (
-            <img
-              title="bookmark trail"
-              alt="bookmark icon"
-              src="https://img.icons8.com/material-outlined/28/fe7762/bookmark-ribbon.png"
-            />
-          )}
+          {hover ? <FaBookmark /> : <FaRegBookmark />}
         </sc.StyledBookmarkIcon>
         {/* TODO: Carousel v2 */}
         <sc.StyledImage
@@ -121,35 +68,30 @@ const TrailCard = ({ trail }) => {
 
         <sc.StyledCardFooter>
           <sc.StyledIconContainer>
-            <img src="https://img.icons8.com/material/18/fe7762/star--v1.png" />
+            <FaRoute />
+            <span title="length in miles" style={{ fontSize: "0.9rem" }}>
+              {Math.floor(trail.length)}
+            </span>
+            {trail.hiking === true && <FaHiking />}
+            {trail.biking === true && <FaBiking />}
+            <sc.StyledBullseye
+              difficulty={trail.difficulty}
+              title={trail.difficulty}
+            />
+          </sc.StyledIconContainer>
+          <sc.StyledIconContainer>
+            <FaMedal />
 
             <span
               title="rating out of 5"
               style={{
-                fontSize: "0.9rem",
+                fontSize: "0.85rem",
                 fontWeight: "bold",
                 color: "#34423B",
               }}
             >
-              {trail.rating === "0" ? "-" : Math.floor(trail.rating)}
+              {trail.rating === "0" ? "" : Math.floor(trail.rating)}
             </span>
-
-            <img
-              title="length in miles"
-              alt="length of trail"
-              src="https://img.icons8.com/pastel-glyph/24/99A29E/route--v2.png"
-            />
-            <span title="length in miles" style={{ fontSize: "0.9rem" }}>
-              {Math.floor(trail.length)}
-            </span>
-            <img
-              title="hiking"
-              alt="hiking icon"
-              src="https://img.icons8.com/material/24/99A29E/trekking.png"
-            />
-          </sc.StyledIconContainer>
-          <sc.StyledIconContainer>
-            {renderDifficultyIcon()}
           </sc.StyledIconContainer>
         </sc.StyledCardFooter>
       </sc.StyledCardContentContainer>
