3f4dcbd213a127ce3fd150a7f67b397ab38a91c2
Tiffani Hess <hesstjune@gmail.com>
2020-12-06 14:32:21 -0600

    brought in different react start component

--- src/pages/scout/StarRating.js
+++ src/pages/scout/StarRating.js
@@ -1,19 +1,25 @@
-import React from "react";
-import ReactStars from "react-rating-stars-component";
+import React, { useState } from "react";
+import StarRatingComponent from "react-star-rating-component";
 
 const StarRating = () => {
-  const ratingChanged = (newRating) => {
-    console.log(newRating);
-  };
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(false);
+
   //TODO: Import own star icon
   return (
-    <ReactStars
-      style={{ display: "inline" }}
-      count={5}
-      onChange={ratingChanged}
+    <StarRatingComponent
+      name={"trail-rating"}
+      starCount={5}
+      onStarClick={(nextValue) => setRating(nextValue)}
+      //   onStarHover={() => setHover(true)}
+      //   onStarHoverOut={(nextValue, prevValue, name) => {
+      //     setHover(false);
+      //   }}
       size={24}
-      color="#99a29e"
-      activeColor="#fe7762"
+      value={rating}
+      //   starColor={hover ? "#fe7762" : "#99a29e"}
+      starColor={"#fe7762"}
+      emptyStarColor={"#99a29e"}
     />
   );
 };
