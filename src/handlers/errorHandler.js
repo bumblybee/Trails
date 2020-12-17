02310f200143d8b35ddb40d9a843a017816d00b5
Tiffani Hess <hesstjune@gmail.com>
2020-12-17 13:11:04 -0600

    added errorHandler and brought into baseApi

---
+++ src/handlers/errorHandler.js
@@ -0,0 +1,22 @@
+const errorCodeToMessage = {};
+
+// Pass array of errors to handleErrors fn
+const handleErrorsArray = (array) => {
+  const errors = array.map(handleErrors);
+  return errors;
+};
+
+// Return error message
+const handleErrors = (errorCode) => {
+  const errorMessage = errorCodeToMessage[errorCode];
+
+  if (errorMessage) {
+    return { error: errorMessage };
+  }
+
+  return {
+    error: "Uh-oh, something is unexpectedly broken. Please contact support.",
+  };
+};
+
+export { handleErrors, handleErrorsArray };
