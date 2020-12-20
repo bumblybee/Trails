cbf91eeb8874003fee51c5952e1967a19e430ff1
Tiffani Hess <hesstjune@gmail.com>
2020-12-20 11:01:09 -0600

    LocationInput dropdown styling, moving location suggestion styling to StyledLocation

--- src/pages/scout/StyledLocation.js
+++ src/pages/scout/StyledLocation.js
@@ -7,18 +7,24 @@ export const StyledLocationInput = styled.input`
 export const StyledLocationDropDown = styled.div`
   z-index: 100;
   position: absolute;
-
+  padding: 0.3rem;
   width: 37.2%;
-  height: 110px;
+  height: 105px;
 
   overflow-y: scroll;
-  /* color: #99a29e;
-  background: #fffeff; */
+  color: #99a29e;
+  background: #fff;
 
   font-size: 0.95rem;
-  box-shadow: 0 0 6px 2px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 1px 6px rgba(0, 0, 0, 0.1);
 
   > div {
-    padding: 0.5rem;
+    padding: 0.3rem;
   }
+`;
+
+export const StyledLocationSuggestion = styled.div`
+  background-color: ${({ suggestion }) =>
+    suggestion.active ? "#eceeed" : "#fff"};
+  cursor: pointer;
 `;
