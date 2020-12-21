92fc5a9505d2f903e42ebed1ef9af19b8a6316e2
Tiffani Hess <hesstjune@gmail.com>
2020-12-21 17:21:50 -0600

    added SuccessConfirmation component and brought into ScoutTrailForm, show on successful submission. Need to treat like modal and style

---
+++ src/components/upload/StyledSuccessConfirmation.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledSuccess = styled.div`
+  text-align: center;
+  padding: 10rem;
+`;
---
+++ src/components/upload/SuccessConfirmation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledSuccess } from "./StyledSuccessConfirmation";
+
+const SuccessConfirmation = () => {
+  return (
+    <StyledSuccess>
+      <h1>Trail Uploaded Successfully!</h1>
+    </StyledSuccess>
+  );
+};
+
+export default SuccessConfirmation;
