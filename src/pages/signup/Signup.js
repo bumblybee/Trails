3a5bf58e201e8d2ccd937ebb54ec9beef26be239
Tiffani Hess <hesstjune@gmail.com>
2021-01-15 16:44:22 -0600

    created user state in signup form, onChange and onSubmit events

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import * as sc from "./StyledSignup";
 
 const Signup = () => {
+  const [userDetails, setUserDetails] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userDetails);
+  };
+
   return (
     <sc.StyledSignupContainer>
-      <sc.StyledForm action="">
+      <sc.StyledForm onSubmit={handleSubmit}>
         <div className="form-header">
           <h2>Join TrailScout</h2>
           <p>
@@ -17,15 +28,41 @@ const Signup = () => {
           <sc.StyledHr />
 
           <label htmlFor="username">Username</label>
-          <input type="text" />
+          <input
+            type="text"
+            id="username"
+            maxLength="50"
+            title="Max characters: 50"
+            onChange={(e) =>
+              setUserDetails({ ...userDetails, username: e.target.value })
+            }
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="email">Email</label>
-          <input type="email" />
+          <input
+            type="email"
+            id="email"
+            onChange={(e) =>
+              setUserDetails({ ...userDetails, email: e.target.value })
+            }
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
-          <input type="password" />
+          <input
+            type="password"
+            id="password"
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+            title="Must contain at least 8 characters with at least one number and one uppercase and lowercase letter."
+            maxLength="50"
+            onChange={(e) =>
+              setUserDetails({ ...userDetails, password: e.target.value })
+            }
+            required
+          />
         </div>
         <button>Join TrailScout</button>
       </sc.StyledForm>
