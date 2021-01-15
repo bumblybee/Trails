916a77c95250fdd8ed5069467edc453bd6212e20
Tiffani Hess <hesstjune@gmail.com>
2021-01-15 16:28:51 -0600

    signup form styling

--- src/pages/signup/StyledSignup.js
+++ src/pages/signup/StyledSignup.js
@@ -2,6 +2,15 @@ import styled from "styled-components";
 
 export const StyledSignupContainer = styled.div`
   padding: 3rem;
+  background: linear-gradient(
+      to bottom,
+      rgba(52, 66, 59, 0.9) 60%,
+      rgba(52, 66, 59, 0.9)
+    ),
+    url("https://images.unsplash.com/photo-1591899761266-148e5928e389?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80");
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 94vh;
 `;
 
 export const StyledForm = styled.form`
@@ -10,14 +19,18 @@ export const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  margin: 9rem auto;
+  justify-content: space-between;
+  margin: 8rem auto;
   border: 1px solid #eceeed;
+  background: #fff;
 
   border-radius: 4px;
   box-shadow: 0 0 4px 4px rgba(0, 0, 0, 0.1);
 
   div.form-group {
+    width: 70%;
+    display: flex;
+    flex-direction: column;
     margin-bottom: 1.3rem;
   }
 
@@ -35,8 +48,9 @@ export const StyledForm = styled.form`
   input {
     border: none;
     background: #eceeed;
-    padding: 0.5rem;
+    padding-left: 0.5rem;
     font-weight: 300;
+    height: 2.4rem;
 
     :focus {
       border: none;
@@ -49,13 +63,14 @@ export const StyledForm = styled.form`
     }
   }
 
-  h1 {
+  h2 {
     margin-top: 0;
-    margin-bottom: 0.25rem;
+    margin-bottom: 0.5rem;
   }
 
   button {
-    padding: 15px 15%;
+    padding: 14px 0;
+    width: 70%;
     margin-top: 1rem;
     background: #fe7762;
     color: #fff;
@@ -75,7 +90,7 @@ export const StyledForm = styled.form`
 `;
 
 export const StyledHr = styled.hr`
-  margin: 1rem 0 2rem;
+  margin: 1rem auto 2rem;
   border: 1px solid #b9c8b8;
   width: 100%;
 `;
