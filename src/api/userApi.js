cfc6466e321b038a520fd576eb0cf286acb014ce
Tiffani Hess <hesstjune@gmail.com>
2021-01-16 15:31:32 -0600

    handling signup error generically for time being

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -3,7 +3,7 @@ import { get, post } from "./baseApi";
 // TODO: create user context and call getUser on app initiation so if logged in, display different nav link options
 export const signupUser = async (data) => {
   const res = await post("/users/signup", data);
-  return res.data;
+  return res;
 };
 
 export const loginUser = async (data) => {
