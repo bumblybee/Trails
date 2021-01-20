006b5be9abb342adb54022ff2de77072e94bbedd
Tiffani Hess <hesstjune@gmail.com>
2021-01-20 16:59:12 -0600

    NavMenu styling and logic to close when linked clicked

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
 import NavMenu from "./NavMenu";
@@ -6,6 +6,11 @@ import * as sc from "./StyledNav";
 
 const Nav = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const menuButtonRef = useRef();
+
+  const closeMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
   return (
     <sc.StyledNav>
@@ -14,11 +19,15 @@ const Nav = () => {
           <sc.StyledLogo src="assets/logo.png" alt="" />
         </Link>
       </div>
-      <NavMenu />
+
+      {showMenu ? <NavMenu closeMenu={closeMenu} /> : ""}
+
       <sc.StyledLinksContainer>
-        <Link to="/account">
-          <FaBars title="account" />
-        </Link>
+        <FaBars
+          title="account"
+          ref={menuButtonRef}
+          onClick={() => setShowMenu(!showMenu)}
+        />
       </sc.StyledLinksContainer>
     </sc.StyledNav>
   );
--- src/components/layout/StyledNavMenu.js
+++ src/components/layout/StyledNavMenu.js
@@ -1,16 +1,43 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const StyledNavMenuContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
-  height: 200px;
   position: absolute;
   z-index: 10;
-  top: 4rem;
-  right: 7.4rem;
+  top: 3.5rem;
+  right: 2.8rem;
   background: #fffeff;
-  padding: 0.5rem 3.5rem 1rem 1rem;
+
   border-radius: 4px;
+  width: 10%;
   box-shadow: 0 1px 6px rgba(0, 0, 0, 0.1);
+`;
+
+export const StyledLink = styled(Link)`
+  color: #34423b;
+  font-size: 0.95rem;
+  padding: 1rem 0 1.25rem 1rem;
+
+  :hover {
+    color: #fffeff;
+    background: #ccc;
+  }
+
+  :first-child {
+    border-top-right-radius: 4px;
+    border-top-left-radius: 4px;
+  }
+
+  :last-child {
+    border-bottom-left-radius: 4px;
+    border-bottom-right-radius: 4px;
+  }
+`;
+
+export const StyledMenuHr = styled.hr`
+  border: none;
+  border-top: 1px solid #b9c8b8;
+  width: 99%;
 `;
