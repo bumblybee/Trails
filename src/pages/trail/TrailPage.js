c6142fb7c7139fe21f791384950fc920882ba88e
Tiffani Hess <hesstjune@gmail.com>
2021-01-31 11:40:33 -0600

    created api call to get single trail, passing trail id as param, calling the api in useEffect at TrailPage

--- src/pages/trail/TrailPage.js
+++ src/pages/trail/TrailPage.js
@@ -1,2 +1,28 @@
 // TODO: add directions to trail via google
 // TODO: v2 - comments, check-in
+
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getSingleTrail, getTrails } from "../../api/trailsApi";
+
+const TrailPage = () => {
+  const { id } = useParams();
+  const [trail, setTrail] = useState({});
+
+  const getTrailData = async () => {
+    const trailData = await getSingleTrail(id);
+    setTrail(trailData);
+  };
+
+  useEffect(() => {
+    getTrailData();
+  }, []);
+
+  return (
+    <div style={{ paddingTop: "3rem" }}>
+      <h1>{trail.name}</h1>
+    </div>
+  );
+};
+
+export default TrailPage;
