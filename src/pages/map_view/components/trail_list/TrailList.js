e019a810299745897d3159f2037506928daa26d3
Tiffani Hess <hesstjune@gmail.com>
2021-02-05 17:55:38 -0600

    created global context state for location search, brought into SearchBar. Trying to use lat, lng, city, state in url

--- src/pages/map_view/components/trail_list/TrailList.js
+++ src/pages/map_view/components/trail_list/TrailList.js
@@ -1,11 +1,19 @@
 import React, { useContext } from "react";
+import { useLocation } from "react-router-dom";
 import { useLocalStorage } from "../../../../hooks/useLocalStorage";
 import { SearchContext } from "../../../../context/search/SearchContext";
 import TrailCard from "./TrailCard";
 import { StyledTrailList, StyledTrailListHeader } from "./StyledTrailList";
 
 const TrailList = ({ setHoveredCard }) => {
-  const { trails } = useContext(SearchContext);
+  const location = useLocation();
+
+  const { trails, searchLocation } = useContext(SearchContext);
+  // window.history.replaceState(
+  //   null,
+  //   null,
+  //   `/search?city=${searchLocation.city}&state=${searchLocation.state}&lat=${searchLocation.coords.lat}&lng=${searchLocation.coords.lng}`
+  // );
   const [locationSearch] = useLocalStorage("location_search");
 
   return (
@@ -15,7 +23,8 @@ const TrailList = ({ setHoveredCard }) => {
           {trails.length ? (
             <h3>
               Trails near{" "}
-              {locationSearch.substring(0, locationSearch.length - 5)}
+              {/* {locationSearch.substring(0, locationSearch.length - 5)} */}
+              {searchLocation.city}, {searchLocation.state}
             </h3>
           ) : (
             <h1>Search a location...</h1>
