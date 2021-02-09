2848ed10d90f8c4411487166626846ba371a803a
Tiffani Hess <hesstjune@gmail.com>
2021-02-08 19:41:35 -0600

    moved BookmarkContext.getUserBookmarks into TrailList and removed from TrailCard so api wasn't called for every card rendered

--- src/context/bookmark/BookmarkProvider.js
+++ src/context/bookmark/BookmarkProvider.js
@@ -1,18 +1,29 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { BookmarkContext } from "./BookmarkContext";
-import { getBookmarks } from "../../api/bookmarkApi";
+import { getBookmarks, createBookmark } from "../../api/bookmarkApi";
 
 const BookmarkProvider = ({ children }) => {
   const [bookmarks, setBookmarks] = useState([]);
 
-  const getUserBookmarks = async (userId) => {
-    const returnedBookmarks = await getBookmarks(userId);
+  const getUserBookmarks = useCallback(
+    async (userId) => {
+      const returnedBookmarks = await getBookmarks(userId);
+      setBookmarks(returnedBookmarks);
+      return bookmarks;
+    },
+    [bookmarks]
+  );
+
+  const createUserBookmark = async (userId, trailId) => {
+    const returnedBookmarks = await createBookmark(userId, trailId);
     setBookmarks(returnedBookmarks);
-    console.log(bookmarks);
+    return bookmarks;
   };
 
   return (
-    <BookmarkContext.Provider value={{ getUserBookmarks, bookmarks }}>
+    <BookmarkContext.Provider
+      value={{ getUserBookmarks, bookmarks, createUserBookmark }}
+    >
       {children}
     </BookmarkContext.Provider>
   );
--- src/pages/map_view/components/trail_list/TrailList.js
+++ src/pages/map_view/components/trail_list/TrailList.js
@@ -1,16 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-
+import { UserContext } from "../../../../context/user/UserContext";
+import { BookmarkContext } from "../../../../context/bookmark/BookmarkContext";
 import { SearchContext } from "../../../../context/search/SearchContext";
 import TrailCard from "./TrailCard";
 import { StyledTrailList, StyledTrailListHeader } from "./StyledTrailList";
 
 const TrailList = ({ setHoveredCard }) => {
   const { trails } = useContext(SearchContext);
+  const { getUserBookmarks } = useContext(BookmarkContext);
+  const { user } = useContext(UserContext);
+
   const location = useLocation();
   const query = new URLSearchParams(location.search);
   const city = query.get("city");
   const state = query.get("state");
+
+  useEffect(() => {
+    user && getUserBookmarks(user.id).then((bkmrks) => console.log(bkmrks));
+  }, []);
 
   return (
     <div>
