47b2cf14e0467758ee37f5641c712f0aafb9f3e3
Tiffani Hess <hesstjune@gmail.com>
2021-02-16 18:23:18 -0600

    styled scouted trail cards and wrapped in link to trail page

--- src/components/layout/cards/StyledTrailCard.js
+++ src/components/layout/cards/StyledTrailCard.js
@@ -1,10 +1,83 @@
 import styled from "styled-components";
 
 export const StyledCardContainer = styled.div`
-  box-shadow: 0 0 1px rgba(0, 0, 0, 0.12);
+  box-shadow: 0 2px 8px 1px rgba(0, 0, 0, 0.12);
+
   border: 0.5px solid #eceeed75;
   border-radius: 6px;
-  padding: 1rem;
-  width: 250px;
+  width: 300px;
+  height: 325px;
+
   background: #fff;
+  margin-right: 1rem;
+
+  display: flex;
+  flex-direction: column;
+
+  :hover {
+    cursor: pointer;
+    box-shadow: 0 0 2px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+export const StyledCardImage = styled.div`
+  width: 100%;
+  height: 150px;
+  background: #ccc;
+
+  img {
+    border-top-left-radius: 6px;
+    border-top-right-radius: 6px;
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const StyledCardContent = styled.div`
+  padding: 1rem 1rem 0.75rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+
+  h4 {
+    text-transform: capitalize;
+    letter-spacing: 0.02rem;
+    margin-top: 0;
+    font-size: 1.2rem;
+  }
+
+  h5 {
+    margin-bottom: 0.75rem;
+    text-transform: capitalize;
+    color: #99a29e;
+    text-shadow: 0 0 1px #99a29e;
+    font-size: 0.9rem;
+    font-weight: 300;
+    font-family: "Noto Sans JP", sans-serif;
+  }
+
+  p {
+    font-weight: 300;
+    margin-bottom: 0.75rem;
+  }
+`;
+
+export const StyledCardFooter = styled.div`
+  margin-top: auto;
+  display: flex;
+  justify-content: space-between;
+  color: #99a29e;
+`;
+
+export const StyledIconContainer = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  font-size: 0.975rem;
+
+  svg {
+    margin-left: 0.507rem;
+  }
 `;
--- src/components/layout/cards/TrailCard.js
+++ src/components/layout/cards/TrailCard.js
@@ -1,13 +1,76 @@
 import React from "react";
+import he from "he";
+import { FaRoute, FaHiking, FaBiking } from "react-icons/fa";
+import StarRating from "../../rating/StarRating";
 import * as sc from "./StyledTrailCard";
+import { StyledCardLinkWrapper } from "../../../pages/map_view/components/trail_list/StyledTrailListCard";
 
 const TrailCard = ({ trail }) => {
   console.log(trail.name);
   return (
-    <sc.StyledCardContainer>
-      <h4>{trail.name}</h4>
-      <p>{trail.description}</p>
-    </sc.StyledCardContainer>
+    <StyledCardLinkWrapper to={`/trail/${trail.id}`}>
+      <sc.StyledCardContainer>
+        <sc.StyledCardImage>
+          <img src={trail.image} alt="" />
+        </sc.StyledCardImage>
+        <sc.StyledCardContent>
+          <div>
+            <h4>{trail.name}</h4>
+            <h5>
+              {trail.city}, {trail.state}
+            </h5>
+            <p>
+              {trail.description.length > 80
+                ? he.decode(trail.description).substring(0, 80) + "..."
+                : he.decode(trail.description)}
+            </p>
+          </div>
+          <sc.StyledCardFooter>
+            <sc.StyledIconContainer>
+              <div
+                title={trail.difficulty}
+                style={{
+                  background:
+                    trail.difficulty === "beginner"
+                      ? "#b1ce7c"
+                      : trail.difficulty === "intermediate"
+                      ? "#fec787"
+                      : trail.difficulty === "advanced"
+                      ? "#FE9787"
+                      : trail.difficulty === "expert"
+                      ? "##FE7762"
+                      : "#eeeeed",
+                  padding: "0.525rem",
+                  width: "14px",
+                  height: "14px",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: "4px",
+                  fontSize: "0.7rem",
+                  fontWeight: "400",
+                  color: "#fff",
+                  border: "1px solid #eeeeed55",
+                }}
+              >
+                {trail.difficulty.charAt(0).toUpperCase()}
+              </div>
+              <FaRoute />
+              <span title="length in miles" style={{ fontSize: "0.8rem" }}>
+                {Math.floor(trail.length)}
+              </span>
+              {trail.hiking === true && <FaHiking />}
+              {trail.biking === true && <FaBiking />}
+            </sc.StyledIconContainer>
+            <sc.StyledIconContainer
+              title={trail.rating === "0" ? "no rating" : "rating"}
+            >
+              <StarRating rating={trail.rating} />
+            </sc.StyledIconContainer>
+          </sc.StyledCardFooter>
+        </sc.StyledCardContent>
+      </sc.StyledCardContainer>
+    </StyledCardLinkWrapper>
   );
 };
 
