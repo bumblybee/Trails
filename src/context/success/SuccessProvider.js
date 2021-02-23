045ce5bf6de2a4ad4e03db9daa1646bd6c2852a9
Tiffani Hess <hesstjune@gmail.com>
2021-02-23 16:00:49 -0600

    created Success Context and component, render when trail edit successfully uploaded, created logic for posting updates to api

---
+++ src/context/success/SuccessProvider.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { SuccessContext } from "./SuccessContext";
+import { toast } from "react-toastify";
+
+const SuccessProvider = ({ children }) => {
+  const [successMessage, setSuccessMessage] = useState(null);
+
+  const setSuccess = (msg) => {
+    toast.success(msg);
+    setSuccessMessage(msg);
+    setTimeout(() => {
+      setSuccessMessage(null);
+    }, 10000);
+  };
+
+  return (
+    <SuccessContext.Provider value={{ successMessage, setSuccess }}>
+      {children}
+    </SuccessContext.Provider>
+  );
+};
+
+export default SuccessProvider;
