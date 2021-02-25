e0e32a512c4017d42b9100436abf3146fc549b39
Tiffani Hess <hesstjune@gmail.com>
2021-02-25 09:53:52 -0600

    added logic to remove bookmarks at TrailCard so user can remove bookmarks from /bookmarks route

--- src/pages/bookmarks/Bookmarks.js
+++ src/pages/bookmarks/Bookmarks.js
@@ -12,14 +12,20 @@ const Bookmarks = () => {
 
   useEffect(() => {
     getUserBookmarks().then((data) => setBookmarks(data));
-  }, []);
+  }, [bookmarks]);
 
   return (
     <sc.StyledBookmarksContainer>
       <h3>Bookmarked Trails</h3>
       <sc.StyledCardContainer>
         {bookmarks && bookmarks.length ? (
-          bookmarks.map((bookmark) => <TrailCard trail={bookmark.trail} />)
+          bookmarks.map((bookmark) => (
+            <TrailCard
+              trail={bookmark.trail}
+              bookmarks={bookmarks}
+              setBookmarks={setBookmarks}
+            />
+          ))
         ) : (
           <h4>No bookmarks to show</h4>
         )}
