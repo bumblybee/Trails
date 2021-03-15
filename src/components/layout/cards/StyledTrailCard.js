6d3e49d1a25a0a43559afc04b398b407e7994daf
Tiffani Hess <hesstjune@gmail.com>
2021-03-15 18:25:11 -0500

    border radius on card image top, if user role = admin then form css positioned higher so can read trail details underneath

--- src/components/layout/cards/StyledTrailCard.js
+++ src/components/layout/cards/StyledTrailCard.js
@@ -33,8 +33,8 @@ export const StyledCardImage = styled.div`
   border-top-right-radius: 6px; */
 
   img {
-    /* border-top-left-radius: 6px;
-    border-top-right-radius: 6px; */
+    border-top-left-radius: 6px;
+    border-top-right-radius: 6px;
     object-fit: cover;
     width: 100%;
     height: 100%;
